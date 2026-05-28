cf27f9cd7fe80bf3826a3230363ca0289b963216
Cristian Chen <111066848+CH3NT@users.noreply.github.com>
2026-05-27 20:03:40 -0600

    Commit 1: Estructura inicial del proyecto en C++ (main)Commit 2: Mejora en mensaje de salida (main)

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hola Mundo desde C++!" << std::endl;
+    std::cout << "Proyecto de demostración de Git y GitHub" << std::endl;
+    std::cout << "Alumno: Cristian Chen" << std::endl;
+    return 0;
+}
